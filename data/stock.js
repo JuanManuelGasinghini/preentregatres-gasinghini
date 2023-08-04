f075b87ae595f42a4be55be3bfc7d48f8855ac1e
JuanManuelGasinghini <113570244+JuanManuelGasinghini@users.noreply.github.com>
2023-08-03 22:08:44 -0300

    Tercer pre-entrega

---
+++ data/stock.js
@@ -0,0 +1,84 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Descarga",
+        tipo: "descarga",
+        desc: "Repuesto para Mochila Capea",
+        precio: 3200,
+        talle: "L",
+        imagen: '../public/images/Descarga.webp',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Descarga con botón",
+        tipo: "descarga",
+        desc: "descarga para Mochila Roca",
+        precio: 5000,
+        talle: "L",
+        imagen: '../public/images/descargaConBoton.webp',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Kit para deposito",
+        tipo: "K",
+        desc: "Kit completo para mochila Ferrum",
+        precio: 3500,
+        talle: "S",
+        imagen: '../public/images/KitParaDeposito.webp',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Bañera",
+        tipo: "bañera",
+        desc: "Bañera Enlozada Medidas:1,50x0,70,050",
+        precio: 60000,
+        talle: "L",
+        imagen: '../public/images/bañera.webp',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Descarga Dual",
+        tipo: "descarga",
+        desc: "Con sistema dual de 6 y 12 litros",
+        precio: 5200,
+        talle: "L",
+        imagen: '../public/images/descargaDual.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Vanitory",
+        tipo: "vanitory",
+        desc: "Mueble y bacha para baño 0.50x1 Metro",
+        precio: 500,
+        talle: "L",
+        imagen: '../public/images/vanitory.jpg',
+        cantidad:1
+    }  
+
+    ];
+
+      const nuevoProducto = {
+        id: 7,
+        nombre: "Mampara",
+        tipo: "mampara",
+        desc: "De vidrio sobre bañera x 0.80",
+        precio: 28000,
+        talle: "M",
+        imagen: "../public/images/mampara.webp",
+        cantidad: 1
+
+      };
+      
+      productos.push(nuevoProducto);
+      
